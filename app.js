a1a4aed1526c23f56c97a28ef989478ad69dc36f
cunbex <hamzalagab.tech@gmail.com>
2024-03-23 20:10:12 +0100

    Remove public and Change disconnect topic

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const createError = require('http-errors');
@@ -14,7 +13,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', adminRouter, controllerRouter);
 
